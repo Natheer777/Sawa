6f14706b05b1deb10933fbc8f91ae196b5fab00b
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-04-01 06:51:42 +0300

    add: deploy work

--- src/App.jsx
+++ src/App.jsx
@@ -180,7 +180,7 @@ function App() {
         <>
           <Router>
             <Routes>
-              <Route path= "/" element={<Home/>}/>
+              <Route path= "/Sawa/" element={<Home/>}/>
             </Routes>
           </Router>
         </>
