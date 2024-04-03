c2b013cbda3a6f78e48a4a01e0d12c1eb706eabd
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-04-03 06:06:21 +0300

    j

--- src/sections/Slogan2/Slogan2.jsx
+++ src/sections/Slogan2/Slogan2.jsx
@@ -18,7 +18,7 @@ export default function Slogan() {
     <>
 
       <div className="cards text-center mb-5 mt-5">
-        <div className="card0 left">{card[6]}<p className="card0">{t("Slogan-card1")}</p></div>
+        <div className="card0 left">{card[6]}<p className="card0">{t("Slogan-card12")}</p></div>
         <div className="card1 left">{card[7]} <p className="card1">{t("Slogan-card5")}</p></div>
         <div className="card2 hidden">{card[8]} <p className="card2">{t("Slogan-card7")}</p> </div>
         <div className="card3 hidden">{card[9]} <p className="card3">{t("Slogan-card6")}</p> </div>
