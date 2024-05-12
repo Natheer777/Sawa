dddb6944dbef63446affbc825ebfc68d84d15896
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-05-12 06:54:32 +0300

    aa

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -1,134 +1,114 @@
-import "./Navbar.css";
-import { useTranslation } from "react-i18next";
-import Logo from "../../assets/Logo/سوىAI-01 (1).webp";
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Logo from '../../assets/Logo/سوىAI-01 (1).webp';
 import { FaHome } from "react-icons/fa";
 import { IoPerson } from "react-icons/io5";
 import {  GrServices } from "react-icons/gr";
 import { FaPhoneAlt } from "react-icons/fa";
 import { LuLanguages } from "react-icons/lu";
-
-
+import "./Navbar.css"
 export default function Navbar() {
-  const [t, i18n] = useTranslation();
+  const { t, i18n } = useTranslation(); // Destructure t (translate function) and i18n (i18next instance)
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language); // Function to change the language using i18n instance
+  };
 
   return (
-    <>
-<div className="container">
-<div className="title container">
-  <ul>
-    <li>  
-          <h1 className=" left">{t("Header-h")}</h1>
-    </li>
-    <li className="diffrint">
-    {t("Header-p")}
-    </li>
-    <li> <a className="navbar-brand" href="#">
-            <img src={Logo} alt="" />
-          </a>
-     </li>
-  </ul>
-    <p className="mt-3 m-0">
-    {/* <marquee direction={i18n.language === 'ar' ? 'right' : 'left'}> */}
-      {/* <h4 className={i18n.language === 'ar' ? 'left' : 'right'}> */}
-    {/* </marquee> */}
-      <div className="typing-demo">
-        <h5 lang={i18n.language}>
-         {t("Main")}
-        </h5>
+    <div className="container">
+      <div className="title container">
+        <ul>
+          <li>
+            <h1 className="left">{t('Header-h')}</h1>
+          </li>
+          <li className="diffrint">{t('Header-p')}</li>
+          <li>
+            <a className="navbar-brand" href="#">
+              <img src={Logo} alt="" />
+            </a>
+          </li>
+        </ul>
+        <p className="mt-3 m-0">
+          <div className="typing-demo">
+            <h5 lang={i18n.language}>
+              {t('Main')}
+            </h5>
           </div>
-      
-
-    </p>
-         
-        </div>
+        </p>
+      </div>
       <nav className="navbar navbar-expand-lg navbar-light mt-4">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse "
-            id="navbarNavDropdown"
-          >
-            <ul className="navbar-nav">
-              <li className="nav-item active item1 hidden">
-                <a className="nav-link" href="#">
-                  <FaHome />{t("Navbar-Home")}
-                  <span className="sr-only"></span>
-                </a>
-              </li>
-              <li className="nav-item item2 hidden">
-                <a className="nav-link" href="#about">
-               <IoPerson />   {t("Navbar-about")}
-                </a>
-              </li>
-              <li className="nav-item item3 hidden">
-                <a className="nav-link" href="#Services">
-              <GrServices />    {t("Navbar-Services")}
-                </a>
-              </li>
-              <li className="nav-item item4 hidden">
-                <a className="nav-link" href="#Contact">
-               <FaPhoneAlt />   {t("Navbar-contact")}
-                </a>
-              </li>
-              <li className="nav-item dropdown item5 hidden">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item active item1">
+              <a className="nav-link" href="#">
+                <FaHome /> {t('Navbar-Home')}
+              </a>
+            </li>
+            <li className="nav-item item2">
+              <a className="nav-link" href="#about">
+                <IoPerson /> {t('Navbar-about')}
+              </a>
+            </li>
+            <li className="nav-item item3">
+              <a className="nav-link" href="#Services">
+                <GrServices /> {t('Navbar-Services')}
+              </a>
+            </li>
+            <li className="nav-item item4">
+              <a className="nav-link" href="#Contact">
+                <FaPhoneAlt /> {t('Navbar-contact')}
+              </a>
+            </li>
+            <li className="nav-item dropdown item5">
+              <a
+                className="nav-link dropdown-toggle fs-5"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+              <LuLanguages />  {/* {t('Navbar-languages')} */}
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                 <a
-                  className="nav-link dropdown-toggle fs-5"
+                  className="dropdown-item"
+                  onClick={() => changeLanguage('ja')}
                   href="#"
-                  id="navbarDropdownMenuLink"
-                  data-bs-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
                 >
-                <LuLanguages />{/* {t("Navbar-languages")} */}
+                  日本語
                 </a>
-                <div
-                  className="dropdown-menu"
-                  aria-labelledby="navbarDropdownMenuLink"
+                <a
+                  className="dropdown-item"
+                  onClick={() => changeLanguage('en')}
+                  href="#"
                 >
-                  <a
-                    onClick={() => {
-                      i18n.changeLanguage("ja");
-                    }}
-                    className="dropdown-item"
-                    // href="/#"
-                  >
-                    日本語
-                  </a>
-                  <a
-                    onClick={() => {
-                      i18n.changeLanguage("en");
-                    }}
-                    className="dropdown-item"
-                    // href="/#"
-                  >
-                    English
-                  </a>
-                  <a
-                    onClick={() => {
-                      i18n.changeLanguage("ar");
-                    }}
-                    className="dropdown-item"
-                    // href="/#"
-                  >
-                    العربية
-                  </a>
-                </div>
-              </li>
-            </ul>
-          </div>
+                  English
+                </a>
+                <a
+                  className="dropdown-item"
+                  onClick={() => changeLanguage('ar')}
+                  href="#"
+                >
+                  العربية
+                </a>
+              </div>
+            </li>
+          </ul>
         </div>
       </nav>
-            </div>
-    </>
+    </div>
   );
 }
